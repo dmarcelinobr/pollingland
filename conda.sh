364cf904d18ae1a361977b9fbcb4dd63552ba4e5
daniel <dmarcelinobr@live.com>
2023-10-23 11:00:53 -0300

    automation

---
+++ conda.sh
@@ -0,0 +1,15 @@
+conda init zsh
+
+conda info | grep -i 'base environment'
+
+conda info --base
+
+cd /Users/marcelino/Documents/pollingland
+
+conda activate /Users/marcelino/Documents/pollingland/env
+
+python --version
+
+conda info | egrep "conda version|active environment"
+
+jupyter notebook --port 8889
